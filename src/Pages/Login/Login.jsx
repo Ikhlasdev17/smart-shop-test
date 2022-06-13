fa872c9d5c3cfdc57c95ba3329cab29d18a7aaf6
Ikhlasdev17 <iklasaralbaev@gmail.com>
2022-06-13 14:39:47 +0500

    testing ios

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,6 +10,7 @@ import NumberFormat from 'react-number-format';
 import { useTranslation } from 'react-i18next';
 
 import { URL } from '../../assets/api/URL';
+import { useRef } from 'react';
 
 
 export function Login() {
@@ -27,10 +28,15 @@ export function Login() {
         }
     }, [])
 
+    const name = useRef(null);
+    const password = useRef(null);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (user.phone !== "" && user.password !== '' && user.pincode !== ''){
-            axios.post(`${URL}/api/login`, user).then(res => {
+            axios.post(`${URL}/api/login`, {
+                phone: name.current.value,
+                password: password.current.value
+            }).then(res => {
                 localStorage.setItem('token', res.data.payload.token)
                 localStorage.setItem('role', res.data.payload.role)
                 localStorage.setItem('user', JSON.stringify(res.data.payload))
@@ -42,7 +48,9 @@ export function Login() {
             .catch(err => {
                 message.error(t('parol_yoki_login_xato'))
             })
-        }
+
+        console.info(name.current.value);
+        console.info(password.current.value);
     }
 
   return (
@@ -55,18 +63,16 @@ export function Login() {
         <div  className="form-label" label={t('telefon_raqami')} name="mobile">
         <input
             style={{padding: '5px'}}
-            mask={"_"}
-            onChange={(e) => setUser({...user, phone: e.target.value})}
             className="input__input"
-            placeholder={t('telefon_raqami')}
-            value={user.phone} 
+            placeholder={'Telefon raqami'}
+            ref={name}
             required
           />
 
         </div>
 
         <div className="form-label" label={t('parol')}>
-            <Input.Password htmlType="password" className="input__input" onChange={(e) => setUser({...user, password: e.target.value})}  placeholder="Password" required/>
+            <input className="input__input"  placeholder="Password" ref={password} required/>
         </div>
         <br />
         <Button htmlType="submit" className="btn btn-primary" style={{width: '100%',}} onClick={handleSubmit}>
