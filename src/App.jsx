be3c71de1e46eb91fd68f1fb95e1ff902f6cce65
Ikhlasdev17 <iklasaralbaev@gmail.com>
2022-06-13 17:49:43 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import Wrapper from './components/Layout/Layout';
-import { Login } from './Pages/Login/Login'; 
-import loadingLogo from '../src/assets/images/SmartShop2.png'
-import axios from 'axios';
-import { setToken, URL } from './assets/api/URL';
-import { userLogout } from './redux/userSlice';
+// import React, { useState, useEffect } from 'react';
+// import { Suspense } from 'react';
+// import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+// import Wrapper from './components/Layout/Layout';
+// import { Login } from './Pages/Login/Login'; 
+// import loadingLogo from '../src/assets/images/SmartShop2.png'
+// import axios from 'axios';
+// import { setToken, URL } from './assets/api/URL';
+// import { userLogout } from './redux/userSlice';
 
-import {useSelector, useDispatch} from 'react-redux'
+// import {useSelector, useDispatch} from 'react-redux'
 
-function App() { 
+// function App() { 
 
 
-  const [loading, setLoading] = useState(true)
-  const [setLogged, log] = useState(true)
-  const [name, setName] = useState('');
+//   const [loading, setLoading] = useState(true)
+//   const [setLogged, log] = useState(true)
+//   const [name, setName] = useState('');
 
-  const dispatch = useDispatch()
+//   const dispatch = useDispatch()
   
-  setTimeout(() => {
-    setLoading(false)
-  }, 1500) 
+//   setTimeout(() => {
+//     setLoading(false)
+//   }, 1500) 
 
-  if (loading) {
+//   if (loading) {
 
-    fetch(`${URL}/api/currency` , setToken())
-    .then(res => {
-      if (res.status === 401) {
-          dispatch(userLogout())
-          setLogged(false)
-      }
-    })
+//     fetch(`${URL}/api/currency` , setToken())
+//     .then(res => {
+//       if (res.status === 401) {
+//           dispatch(userLogout())
+//           setLogged(false)
+//       }
+//     })
 
-    return <div className="overlay">
-    <img src={loadingLogo} alt="" />
-  </div>
-  }
+//     return <div className="overlay">
+//     <img src={loadingLogo} alt="" />
+//   </div>
+//   }
  
   
-  return (
-    <div className="App">
+//   return (
+//     <div className="App">
       
-     {/* <Router>
-            <Routes>
-                  <Route path="/*" element={<Wrapper />} />
-                  <Route path="/login" element={<Login />} />
-            </Routes>
-    </Router> */}
+//      {/* <Router>
+//             <Routes>
+//                   <Route path="/*" element={<Wrapper />} />
+//                   <Route path="/login" element={<Login />} />
+//             </Routes>
+//     </Router> */}
 
-    <input value={name} onChange={e => setName(e.target.value) } placeholder="Enter your name..." />
-    </div>
+//     <input value={name} onChange={e => setName(e.target.value) } placeholder="Enter your name..." />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+const App = () => {
+  const [name, setName] = React.useState('');
+  return (
+    <div><input value={name} onChange={e => setName(e.target.value) } placeholder="Enter your name..." /><br /> {name}</div>
   )
 }
 
